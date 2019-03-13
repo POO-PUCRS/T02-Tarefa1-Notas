ba127fe9518a635fc765bd35a29cfeb0cc0ca6c3
Bernardo Copstein <bernardo.copstein@pucrs.br>
2019-03-13 09:54:16 -0300

    Alteracao no App

--- App.java
+++ App.java
@@ -12,6 +12,15 @@ public class App{
         for(int i=0;i<n.getQtdade();i++){
             System.out.println(n.getNota(i));
         }
-        System.out.println(n.getNotaFinal());
+        System.out.println("Nota final: "+n.getNotaFinal());
+        System.out.println("----------------------");
+        n.infoNota(7);
+        n.infoNota(3);
+        n.calculaNotaFinal();
+        System.out.println("Notas consideradas: ");
+        for(int i=0;i<n.getQtdade();i++){
+            System.out.println(n.getNota(i));
+        }
+        System.out.println("Nota final: "+n.getNotaFinal());
     }
 }
